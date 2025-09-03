c9ce0bde655c33f6f96787b14d4adc91e4c20e85
Yuritzi Calixto <ramirezcalixtodiana@gmail.com>
2025-09-02 19:58:15 -0600

    Plantilla administrador

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    HOLA MUNDO
+</x-admin-layout>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Styles -->
+    @livewireStyles
+</head>
+<body>
+
+{{-- START PLANTILLA --}}
+
+@include('layouts.includes.admin.navigation')
+@include('layouts.includes.admin.sidebar')
+
+<div class="p-4 sm:ml-64">
+   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+      {{$slot}}
+   </div>
+</div>
+
+{{-- END PLANTILLA --}}
+{{-- Agregamos el stack modals --}}
+    @stack('modals')
+
+    @livewireScripts
+</body>
+</html>
